ba6bdf8edb45a3e4221cf658cc137bfcfcf04db8
Barış Özgür Yaşar <yasarbarisozgur@gmail.com>
2023-10-10 17:51:48 +0300

    BlogAppFirstPart

--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -1,31 +1,65 @@
-import "./topbar.css"
+import { Link } from 'react-router-dom';
+import './topbar.css';
 
 export default function TopBar() {
+  const user = false;
   return (
-    <div className='top'>
-        <div className="topLeft">
-            <i className="topIcon fa-brands fa-square-facebook"></i> 
-            <i className="topIcon fa-brands fa-square-instagram"></i>
-            <i className="topIcon fa-brands fa-square-twitter"></i>
-            <i className="topIcon fa-brands fa-square-pinterest"></i>
-        </div>
-        <div className="topCenter">
-            <ul className="topList">
-                <li className="topListItem">HOME</li>
-                <li className="topListItem">ABOUT</li>
-                <li className="topListItem">CONTACT</li>
-                <li className="topListItem">WRITE</li>
-                <li className="topListItem">LOGOUT</li>
-            </ul>
-        </div>
-        <div className="topRight">
-            <img 
+    <div className="top">
+      <div className="topLeft">
+        <i className="topIcon fa-brands fa-square-facebook"></i>
+        <i className="topIcon fa-brands fa-square-instagram"></i>
+        <i className="topIcon fa-brands fa-square-twitter"></i>
+        <i className="topIcon fa-brands fa-square-pinterest"></i>
+      </div>
+      <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">
+            <Link className="link" to="/">
+              HOME
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link className="link" to="/about">
+              ABOUT
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link className="link" to="/contact">
+              CONTACT
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link className="link" to="/write">
+              WRITE
+            </Link>
+          </li>
+          <li className="topListItem">{user && 'LOGOUT'}</li>
+        </ul>
+      </div>
+      <div className="topRight">
+        {user ? (
+          <img
             className="topImg"
-            src="https://sm.ign.com/t/ign_tr/news/b/baldurs-ga/baldurs-gate-3-pc-release-date-brought-forward-a-month-avoid_6bm4.1280.jpg" 
-            alt="" 
-            />
-            <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
-        </div>
+            src="https://sm.ign.com/t/ign_tr/news/b/baldurs-ga/baldurs-gate-3-pc-release-date-brought-forward-a-month-avoid_6bm4.1280.jpg"
+            alt=""
+          />
+        ) : (
+          <ul className="topList">
+            <li className="topListItem">
+              <Link className="link" to="/login">
+                LOGIN
+              </Link>
+            </li>
+            <li className="topListItem">
+              <Link className="link" to="/register">
+                REGISTER
+              </Link>
+            </li>
+          </ul>
+        )}
+
+        <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
+      </div>
     </div>
   );
 }
